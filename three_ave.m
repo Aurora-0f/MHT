1e6088ebaf6fe1ec95612d29b75bebb19ed5742e
Aurora-0f <88010557+Aurora-0f@users.noreply.github.com>
2023-07-20 10:24:41 +0800

    three_ave.m

--- three_ave.m
+++ three_ave.m
@@ -1,13 +1,43 @@
 tic
-Seg_L=imread('aa\L.png');
-Seg_M=imread('aa\M.png');
-Seg_H=imread('aa\H.png');
-GT=imread('aa\test-gt.png');
+Seg_L=imread('L.png');
+Seg_M=imread('M.png');
+Seg_H=imread('H.png');
+GT=imread('gt.png');
 
-Seg=(Seg_L+Seg_M+Seg_H)/3;
-figure;imshow(Seg);
+% Seg=(Seg_L+Seg_M+Seg_H)/3;
 
 % [ GT ] = correctGT( GT);
+
+labelL=bwlabel(Seg_L,4);
+numObjectsL = max(labelL(:));
+for i = 1 : numObjectsL
+    objectL = labelL == i;
+    L_edge{i} = bwboundaries(objectL);
+%     figure;imshow(L_edge{i},[]);
+end
+
+labelM=bwlabel(Seg_M,4);
+numObjectsM = max(labelM(:));
+for i = 1 : numObjectsM
+    objectM = labelM == i;
+    M_edge{i} = bwboundaries(objectM);
+%     figure;imshow(L_edge,[]);
+end
+
+labelH=bwlabel(Seg_H,4);
+numObjectsH = max(labelH(:));
+for i = 1 : numObjectsH
+    objectH = labelH == i;
+    H_edge{i} = bwboundaries(objectH);
+%     figure;imshow(L_edge,[]);
+end
+
+num = min(min(numObjectsL,numObjectsM),numObjectsH);
+for i= 1 : num
+    edge{i} = (L_edge{i}{1,1}+M_edge{i}{1,1}+H_edge{i}{1,1})./3;
+end
+Seg = edge;
+
 % CC_Seg_L=bwconncomp(Seg_L,4);
 % S_Seg_L=regionprops(CC_Seg_L,Seg_L,'Centroid','PixelIdxList','BoundingBox');
 % 
@@ -31,8 +61,6 @@ figure;imshow(Seg);
 % k_I1=length(S_Seg_I1);
 % k=min(k2,k_I1);
 % I_new = seg_ave(k,S_Seg_M,S_Seg_I1,I1,I);
-
-% figure;imshow(I_new,[]);
 
 % Seg=I_new;
 
